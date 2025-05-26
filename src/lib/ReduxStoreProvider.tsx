fa8f24998ed800bd28e7434cecf69eb75b0a5e77
Gratien Tuyishimire <gratientuyishimire@gmail.com>
2025-05-26 08:11:32 +0200

    feat: husky and prettier

--- src/lib/ReduxStoreProvider.tsx
+++ src/lib/ReduxStoreProvider.tsx
@@ -1,13 +1,13 @@
-"use client";
-import React from "react";
-import { Provider } from "react-redux";
-import { store, type AppStore } from "@/redux/Store";
+'use client';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store, type AppStore } from '@/redux/Store';
 
 type Props = {
   children: React.ReactNode;
 };
 export const ReduxStoreProvider = ({ children }: Props) => {
-  const storeRef = React.useRef<AppStore>(undefined);
+  const storeRef = React.useRef<AppStore | undefined>(undefined);
 
   if (!storeRef.current) {
     storeRef.current = store();
